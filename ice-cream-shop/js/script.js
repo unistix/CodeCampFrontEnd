fcc967332a21f8e2248eb4df81dcb0bfc6da4ddc
Cynny <c.dafinone@gmail.com>
2022-09-09 19:54:18 +0100

    finished ice cream website

--- ice-cream-shop/js/script.js
+++ ice-cream-shop/js/script.js
@@ -1,4 +1,24 @@
-console.log("test");
+//console.log("test");
+
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+	menuBtn.classList.toggle('fa-times');
+	navbar.classList.toggle('active');
+
+}
+
+
+
+window.onscroll = () =>{
+	menuBtn.classList.remove('fa-times');
+	navbar.classList.remove('active');
+
+}
+
+
+//Single Full Image slider
 
 var swiper = new Swiper(".home-slider",{
 	grabCursor:true,
@@ -21,3 +41,32 @@ var swiper = new Swiper(".home-slider", {
     },
 });*/
 
+//Grid Gallery Image slider
+
+var swiper = new Swiper(".reviews-slider", {
+	pagination:{
+		el:".swiper-pagination",
+		clickable:true,
+	},
+	grabCursor:true,
+	loop:true,
+	spaceBetween:20,
+	breakpoints:{
+		0:{
+
+			slidesPerView:1,
+
+		},
+		768:{
+
+			slidesPerView:2,
+
+		},
+		768:{
+
+			slidesPerView:3,
+
+		},
+	}
+
+})
